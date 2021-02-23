a35d1844d72690cef8ea0f6f847dba2624fdea48
trasparente <stradatrasparente@gmail.com>
2021-02-23 17:45:58 +0100

    Fix modal image

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,13 +26,48 @@ $(function() {
   }
 
   // Image zoom on Posts
-  $('body.post article > figure > img').click(function(e){
-    // var ele = $(e.target);
-    // ele.toggleClass('mega');
-    $('#zoom').attr("open", "");
-  });
-  $('#zoom').click(function(e){
-    $(e.delegateTarget).removeAttr('open');
-  })
+  // $('body.post article > figure > img').click(function(e){
+  //   // var ele = $(e.target);
+  //   // ele.toggleClass('mega');
+  //   $('#zoom').attr("open", "");
+  // });
+  // $('#zoom').click(function(e){
+  //   $(e.delegateTarget).removeAttr('open');
+  // })
 
+  // MODAL
+  // Get the modal
+  var modal = document.getElementById("myModal");
+  var modal_window = document.querySelector(".modal-content");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  // var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks the button, open the modal 
+  if (btn){
+    btn.onclick = function(e) {
+      e.preventDefault();
+      modal.style.display = "block";
+    }
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  // span.onclick = function() {
+  //   modal.style.display = "none";
+  // }
+
+  // When the user clicks anywhere outside of the modal, close it
+  if (modal_window) {
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+    modal_window.onclick = function(event) {
+      modal.style.display = "none";
+    }
+  }
 });
